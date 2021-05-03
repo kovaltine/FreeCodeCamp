338403e6448ec7fdeef82b72b9ff34b164fe0c6f
Kira <58193024+kovaltine@users.noreply.github.com>
2021-05-03 11:53:14 -0400

    Product Landing Page for portfolio

---
+++ ProductLandingPage/app.js
@@ -0,0 +1,22 @@
+// this is for the burger menu
+
+//navigation slides from the right to the left
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    //when we click nav-links, we want it to get nav-active
+    const nav = document.querySelector('.nav-link');
+    const navLinks = document.querySelectorAll('.nav-link li');
+
+    //toggle nav
+    burger.addEventListener('click', () => {
+        //switches to a different class
+        nav.classList.toggle('nav-active');
+        //animate links
+        navLinks.forEach((link, index) => {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`
+        });
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
